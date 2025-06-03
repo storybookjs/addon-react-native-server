be10b3e44d0ba0e0b3e1cc21cba0e2b7e42a14ee
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-06-03 14:21:56 +0100

    update to use more react native like setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react-vite";
+import type { Preview } from "@storybook/react-native-web-vite";
 
 const preview: Preview = {
   parameters: {
--- src/preview.tsx
+++ src/preview.tsx
@@ -1,5 +1,4 @@
 import type { ProjectAnnotations, Renderer } from "storybook/internal/types";
-import React from "react";
 
 /**
  * Note: if you want to use JSX in this file, rename it to `preview.tsx`
